14997eb76db610bc866361e308814938cade65f0
vanhytt <leevanh2006@gmail.com>
2026-06-10 08:58:00 +0700

    đổi domain chính cho web

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://yuna-web.vercel.app'
+  const baseUrl = 'https://yunavietnam.com'
   
   return [
     {
